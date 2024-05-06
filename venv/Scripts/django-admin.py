d4c23bc5b8edbd616e9e7c9968b213af0dbc59ad
sowmyanarayanan96 <svaradharajan@randomtrees.com>
2024-05-06 07:52:39 +0530

    setup django project

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lenovo\desktop\new folder\django_ml\django_ml\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
